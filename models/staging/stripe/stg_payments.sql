3a63eabe94868c2817e9a644c1419c715bd1d827
adrian98887@gmail.com <adrian98887@gmail.com>
2023-04-23 12:06:48 +0000

    fix1

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -2,6 +2,6 @@ select id as payment_id,
     orderid as order_id, 
     paymentmethod as payment_method, 
     status, amount / 100 as amount, 
-c   reated as created_as
+    created as created_as
 
 from raw.stripe.payment
